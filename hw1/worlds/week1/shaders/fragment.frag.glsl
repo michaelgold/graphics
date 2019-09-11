3626af36fe15c214088a081403eae6935647eb13
Michael Gold <goldmichael@gmail.com>
2019-09-11 16:23:12 -0400

    updated week1 fragment shader

--- hw1/worlds/week1/shaders/fragment.frag.glsl
+++ hw1/worlds/week1/shaders/fragment.frag.glsl
@@ -13,13 +13,14 @@ void main() {
   // HERE YOU CAN WRITE ANY CODE TO
   // DEFINE A COLOR FOR THIS FRAGMENT
 
-  float red   = max(0., vPos.x);
-  float green = max(0., vPos.y);
-  float blue  = max(0., sin(5. * uTime));
+  float red   = noise((4. * vPos - uTime/4.) + sin(vec3(2.,4.,0.)));
+  float green = noise((2.3 * vPos - uTime/3.) + sin(vec3(2.,1.,.2)));
+  float blue  = noise((1.5 * vPos - uTime/3.) + sin(vec3(2.,3.,.2)));
   
   // R,G,B EACH RANGE FROM 0.0 TO 1.0  
-  vec3 color = vec3(red, green, blue);
-    
+  vec3 color = vec3(red, green, blue) ;
+  
+
   // THIS LINE OUTPUTS THE FRAGMENT COLOR
   fragColor = vec4(sqrt(color), 1.0);
 }
