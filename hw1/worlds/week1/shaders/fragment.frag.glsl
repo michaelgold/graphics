c61d92a1396adfc3f44e1545f2bced9ac4a89415
Michael Gold <goldmichael@gmail.com>
2019-09-11 17:27:25 -0400

    made purple lava lamp

--- hw1/worlds/week1/shaders/fragment.frag.glsl
+++ hw1/worlds/week1/shaders/fragment.frag.glsl
@@ -13,12 +13,11 @@ void main() {
   // HERE YOU CAN WRITE ANY CODE TO
   // DEFINE A COLOR FOR THIS FRAGMENT
 
-  float red   = noise((4. * vPos - uTime/4.) + sin(vec3(2.,4.,0.)));
-  float green = noise((2.3 * vPos - uTime/3.) + sin(vec3(2.,1.,.2)));
+  float red = noise((1.5 * vPos - uTime/3.) + sin(vec3(2.,3.,.2)));
   float blue  = noise((1.5 * vPos - uTime/3.) + sin(vec3(2.,3.,.2)));
   
   // R,G,B EACH RANGE FROM 0.0 TO 1.0  
-  vec3 color = vec3(red, green, blue) ;
+  vec3 color = vec3(red, 0, blue) ;
   
 
   // THIS LINE OUTPUTS THE FRAGMENT COLOR
