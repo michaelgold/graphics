5301d0370595da887c517b2b8b16ffe4812f4574
Michael Gold <goldmichael@gmail.com>
2019-09-19 15:58:20 -0400

    initialize spheres and lights

--- hw2/worlds/week2/shaders/fragment.frag.glsl
+++ hw2/worlds/week2/shaders/fragment.frag.glsl
@@ -2,10 +2,32 @@
 precision highp float; // HIGH PRECISION FLOATS
 
 uniform float uTime;   // TIME, IN SECONDS
-in vec3 vPos;          // POSITION IN IMAGE
-out vec4 fragColor;    // RESULT WILL GO HERE
+
+
+const int NS = 2; // Number of spheres in the scene
+const int NL = 2; // Number of light sources in the scene
+
+// Declarations of arrays for spheres, lights and phong shading:
+
+vec3 Ldir[NL], Lcol[NL], Ambient[NS], Diffuse[NS];
+vec4 Sphere[NS], Specular[NS];
 
 void main() {
-    vec3 color = cos(100. * vPos);
-    fragColor = vec4(sqrt(color), 1.0);
+    Ldir[0] = normalize(vec3(1.,1.,.5));
+    Lcol[0] = vec3(1.,1.,1.);
+
+    Ldir[1] = normalize(vec3(-1.,0.,-2.));
+    Lcol[1] = vec3(.1,.07,.05);
+
+    Sphere[0]   = vec4(.2,0.,0.,.3);
+    Ambient[0]  = vec3(0.,.1,.1);
+    Diffuse[0]  = vec3(0.,.5,.5);
+    Specular[0] = vec4(0.,1.,1.,10.); // 4th value is specular power
+
+    Sphere[1]   = vec4(-.6,.4,-.1,.1);
+    Ambient[1]  = vec3(.1,.1,0.);
+    Diffuse[1]  = vec3(.5,.5,0.);
+    Specular[1] = vec4(1.,1.,1.,20.); // 4th value is specular power
+
+    
 }
