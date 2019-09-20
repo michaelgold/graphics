9f5b4041b4aa83ef340e71eca7deae9068ccc507
Michael Gold <goldmichael@gmail.com>
2019-09-19 22:58:48 -0400

    spheres displaying w specular highlights w green color

--- hw2/worlds/week2/shaders/fragment.frag.glsl
+++ hw2/worlds/week2/shaders/fragment.frag.glsl
@@ -13,17 +13,20 @@ const int NL = 2; // Number of light sources in the scene
 
 // Declarations of arrays for spheres, lights and phong shading:
 
-vec3 Ldir[NL], Lcol[NL], Ambient[NS], Diffuse[NS], V, W, vPrime, P;
+vec3 Ldir[NL], Lcol[NL], Ambient[NS], Diffuse[NS], V, W, VV, P, N, color;
 vec4 Sphere[NS], Specular[NS];
 float t;
-float fl = 1.0;
+float fl = 4.;
 
 
 float raySphere(vec3 V, vec3 W, vec4 S) {
+    VV = V - S.xyz;
+    return -(dot(W,VV)) - sqrt(pow(dot(W,VV), 2.0) - dot(VV,VV) + pow(S.w, 2.0));
+}
 
-    vPrime = V - S.xyz;
-    return -(dot(W,vPrime)) - sqrt(pow(dot(W,vPrime), 2.0) - dot(vPrime,vPrime) + pow(S.w, 2.0) );
-
+bool isInShadow(vec3 P, vec3 L) {
+    return false;
+    
 }
 
 void main() {
@@ -33,12 +36,12 @@ void main() {
     Ldir[1] = normalize(vec3(-1.,0.,-2.));
     Lcol[1] = vec3(.1,.07,.05);
 
-    Sphere[0]   = vec4(.2,0.,0.,.3);
+    Sphere[0]   = vec4(.2,0.,0.,.63);
     Ambient[0]  = vec3(0.,.1,.1);
     Diffuse[0]  = vec3(0.,.5,.5);
     Specular[0] = vec4(0.,1.,1.,10.); // 4th value is specular power
 
-    Sphere[1]   = vec4(-.6,.4,-.1,.1);
+    Sphere[1]   = vec4(-.6,.4,-.1,.41);
     Ambient[1]  = vec3(.1,.1,0.);
     Diffuse[1]  = vec3(.5,.5,0.);
     Specular[1] = vec4(1.,1.,1.,20.); // 4th value is specular power
@@ -46,13 +49,39 @@ void main() {
 
     for (int i = 0; i < Sphere.length(); i++) {
         
-        
+        V = vec3(0,0,fl);
         W = normalize(vec3(vPos.x, vPos.y, -fl));
-        t = raySphere(vPos, W, Sphere[i]);
-        P = V + t * W;
+        t = raySphere(V, W, Sphere[i]);
+        float tMin = 1000.;
+        if (t > 0. && t < tMin) {
+            P = V + t * W;
+            N = normalize(P - Sphere[i].w);      
+            tMin = t;
+        }
+        
+        
+        vec3 E = -W;
+
+        
+        vec3 specularComponent = vec3(0.,0.,0.);
+        vec3 diffuseComponent = vec3(0.,0.,0.);
+
+        for (int j = 0; j < Lcol.length(); j++) {
+            vec3 R = 2. * N * dot(N,Ldir[j]) - Ldir[j];
+            if (!isInShadow(P, Ldir[j])){
+                specularComponent += Specular[i].rgb * pow(max(0., dot(E,R)), Specular[i].w);
+                diffuseComponent += Diffuse[i].rgb * max(0., dot(N,Lcol[j]));
+            }
+
+        }
+
+        color +=  Ambient[i] + diffuseComponent + specularComponent;
 
 
-        fragColor = vec4(sqrt(P), 1.0);
+        
+        
+
+        fragColor = vec4(sqrt(color), 1.0);
 
     }
     
